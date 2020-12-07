86e4fc7acf9f5ad0d2cfe11bd4604d1308516466
jckim9203 <jckim9203@gmail.com>
2020-12-07 17:30:30 +0900

    Add files via upload

---
+++ BucketFactory.py
@@ -0,0 +1,36 @@
+import EncoderFactory
+
+import ClusterBasedBucketer
+import StateBasedBucketer
+import PrefixLengthBucketer
+import NoBucketer
+from sklearn.cluster import KMeans
+from sklearn.cluster import SpectralClustering
+from sklearn.cluster import AgglomerativeClustering
+
+        
+def get_bucketer(method, encoding_method=None, case_id_col=None, cat_cols=None, num_cols=None, n_clusters=None, random_state=None, n_neighbors=None):
+
+    if method == "cluster":
+        bucket_encoder = EncoderFactory.get_encoder(method=encoding_method, case_id_col=case_id_col, dynamic_cat_cols=cat_cols, dynamic_num_cols=num_cols)
+        #clustering = KMeans(n_clusters, random_state=random_state) #Change it to AffinityPropagation, MeanShift, SpectralClustering, DBSCAN, AgglomerativeClustering or other clustering techniques if necessary 
+        clustering = SpectralClustering(n_clusters, random_state=random_state, n_init = 30) #Change it to AffinityPropagation, MeanShift, SpectralClustering, DBSCAN, AgglomerativeClustering or other clustering techniques if necessary 
+        return ClusterBasedBucketer(encoder=bucket_encoder, clustering=clustering)
+        
+    elif method == "state":
+        bucket_encoder = EncoderFactory.get_encoder(method=encoding_method, case_id_col=case_id_col, dynamic_cat_cols=cat_cols, dynamic_num_cols=num_cols)
+        return StateBasedBucketer(encoder=bucket_encoder)
+            
+    elif method == "single":
+        return NoBucketer(case_id_col=case_id_col)
+
+    elif method == "prefix":
+        return PrefixLengthBucketer(case_id_col=case_id_col)
+    
+    elif method == "knn":
+        bucket_encoder = EncoderFactory.get_encoder(method=encoding_method, case_id_col=case_id_col, dynamic_cat_cols=cat_cols, dynamic_num_cols=num_cols)
+        return KNNBucketer(encoder=bucket_encoder, n_neighbors=n_neighbors)
+
+    else:
+        print("Invalid bucketer type")
+        return None
